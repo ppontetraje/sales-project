7f0d927a7988398c18135c53c42112f44183ea85
Jean Piert Palomino Quispe <dragon3706@hotmail.com>
2023-03-21 17:54:09 -0500

    Cors configuration

--- Sales/Sales.API/Program.cs
+++ Sales/Sales.API/Program.cs
@@ -27,4 +27,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
+
 app.Run();
